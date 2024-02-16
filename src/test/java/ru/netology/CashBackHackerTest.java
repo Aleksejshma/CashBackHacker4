b0f60b71a879fefff7c868c1851e7e003e417177
Alexsej <aleksejshmakov7@gmail.com>
2024-02-16 23:27:26 +0300

    commit 3

--- src/test/java/ru/netology/CashBackHackerTest.java
+++ src/test/java/ru/netology/CashBackHackerTest.java
@@ -1,5 +1,34 @@
 package ru.netology;
-
+import static org.testng.Assert.assertEquals;
 public class CashBackHackerTest {
-    
+
+    @org.testng.annotations.Test
+    public void testRemain ()
+    {
+        CashBackHacker service = new CashBackHacker();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(actual,expected);
+    }
+    @org.testng.annotations.Test
+    public void testreminWitchCashback ()
+    {
+        CashBackHacker service = new CashBackHacker();
+        int amount = 900;
+        int actual = service.remain(amount);
+        int expected = 100;
+        assertEquals(actual,expected);
+    }
+
+    @org.testng.annotations.Test
+    public void testremiZero ()
+    {
+        CashBackHacker service = new CashBackHacker();
+        int amount = 0;
+        int actual = service.remain(amount);
+        int expected = 1000;
+        assertEquals(actual,expected);
+    }
+
 }
